a5f047777b902d0f086d02ab2a5a8f316698b021
Fernando Bráulio <44295487+Fernando-Braulio@users.noreply.github.com>
2020-04-30 22:49:43 -0300

    editing a blog article in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fortawesome';
+// import {faEdit} from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 
 function App() {
+  const [ content, setContent ] = useState({
+    title: 'Hello OWord',
+    section1: {
+      title: 'Section',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since'
+    }
+  });
+  const [editing, setEditing] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="App-Header">
+        <div></div>
+        {!editing 
+          ? <hi>{content.title}</hi>
+          : <input type="text"
+              value={content.title}
+              onChange={e => setContent({ ...content, title: e.target.value })}
+              className="liveEditH1"/>
+        }
+        <p className="editBtn" onClick={() => setEditing(!editing)}>
+          {!editing ? 'Edit' : 'Publish'}
+          {/* <FontAwesomeIcon icon={faEdit} className="icon" /> */}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="App-Body">
+        
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
